5cbb1533edc4fb548153823bf722279244f6b36c
liza <lizaveta2011@tut.by>
2023-06-25 18:48:02 +0300

    2w

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 > number2){
+    Console.WriteLine(number1);
+}
+else
+    if(number1<number2){
+         Console.WriteLine(number2);
+  } 
+   else
+   if(number1==number2){
+    Console.WriteLine("Оба числа ровны");
+    } 
