4a872d603328a5b45b42185620f714c0182ddfb6
liza <lizaveta2011@tut.by>
2023-06-25 22:02:58 +0300

    4

---
+++ 4/Program.cs
@@ -0,0 +1,33 @@
+﻿﻿Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (num1 > num2)
+{
+    max = num1;
+    if (max > num3)
+    {
+        Console.WriteLine($"Максимальное число {max}");
+    }
+    else
+    {
+        max = num3;
+        Console.WriteLine($"Максимальное число {max}");
+    }
+}
+else
+{
+    max = num2;
+    if (max > num3)
+    {
+     Console.WriteLine($"Максимальное число {max}");
+    }
+    else
+    {
+        max = num3; 
+        Console.WriteLine($"Максимальное число {max}");
+    }
+}
