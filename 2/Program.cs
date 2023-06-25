32b6a8dce75309a846cc52a2962912e4c3a45c5b
liza <lizaveta2011@tut.by>
2023-06-25 22:36:23 +0300

    222

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,16 +1,18 @@
-﻿﻿Console.Clear();
-Console.Write("Введите первое число: ");
+﻿﻿Console.Write("Введите первое число: ");
 int number1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine()!);
-if(number1 > number2){
-    Console.WriteLine(number1);
+if (number1 > number2)
+{
+    Console.WriteLine($"Max = {number1} Min = {number2}");
 }
 else
-    if(number1<number2){
-         Console.WriteLine(number2);
-  } 
-   else
-   if(number1==number2){
+    if (number1 < number2)
+{
+    Console.WriteLine($"Max = {number2} Min = {number1}");
+  }
+else
+   if (number1 == number2)
+{
     Console.WriteLine("Оба числа равны");
-    } 
+}
