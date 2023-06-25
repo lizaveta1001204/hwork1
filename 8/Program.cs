13fbb1baf4cd5f7a1e2f54e26b414b1487d5dd69
liza <lizaveta2011@tut.by>
2023-06-25 22:44:55 +0300

    8

---
+++ 8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine()!);
+int n = 2;
+while (n <= num)
+    {
+        Console.Write($"{n}, ");
+        n+=2;
+    }
